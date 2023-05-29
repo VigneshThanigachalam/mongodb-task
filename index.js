86c5b9ba388041eb6ab33a2979cefc8675ab5936
VigneshThanigachalam <82868586+VigneshThanigachalam@users.noreply.github.com>
2023-05-29 16:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,59 +18,59 @@ const productModel = mongoose.model('products', productSchema);
 
 const allProducts= await productModel.find({})
 
-// console.log(allProducts);
+ console.log(allProducts);
 
 
 // Find the product price which are between 400 to 800
 
 const productBetween = await productModel.find({product_price:{$gte: 400, $lte:800}});
 
-// console.log(productBetween);
+ console.log(productBetween);
 
 
 // Find the product price which are not between 400 to 600
 
 const productNotBetween = await productModel.find({product_price:{ $not: {$gte: 400, $lte:800}}});
 
-// console.log(productNotBetween);
+console.log(productNotBetween);
 
 // List the four product which are grater than 500 in price 
 
 const productFour = await productModel.find({product_price:{$gt: 500}}).limit(4);
 
-// console.log(productFour);
+ console.log(productFour);
 
 // Find the product name and product material of each products
 
 const productNameAndMaterial = await productModel.find({}).select("product_name product_material");
 
-// console.log(productNameAndMaterial);
+console.log(productNameAndMaterial);
 
 
 // Find the product with a row id of 10
 
 const productWithId = await productModel.find({id:"10"});
 
-// console.log(productWithId);
+ console.log(productWithId);
 
 
 // Find only the product name and product material
 
 const productNameAndMaterialOnly = await productModel.find({}).select("product_name product_material -_id");
 
-// console.log(productNameAndMaterialOnly);
+ console.log(productNameAndMaterialOnly);
 
 // Find all products which contain the value of soft in product material
 
 const productMaterialSoft = await productModel.find({product_material: "Soft"});
 
-// console.log(productMaterialSoft);
+ console.log(productMaterialSoft);
 
 // Find products which contain product color indigo  and product price 492.00
 
 const productIndigoAndPrice = await productModel.find({$and: [{product_color: "indigo"}, {product_price: 492.00}]});
 
-// console.log(productIndigoAndPrice);
+ console.log(productIndigoAndPrice);
 
 // Delete the products which product price value are same
 
